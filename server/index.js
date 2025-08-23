d2772af0b51ae35a1df24805ed3ae2471cca2636
Atharv <visputeatharvvispute@gmail.com>
2025-08-23 12:04:20 +0530

    just updated

--- server/index.js
+++ server/index.js
@@ -1,1 +1,14 @@
-"console.log('Server running');" 
+// Backend test server with Express
+const express = require("express");
+const app = express();
+const PORT = 5000;
+
+app.get("/", (req, res) => {
+  res.send("Hello from Express backend!");
+});
+
+app.listen(PORT, () => {
+  console.log(`✅ Server is running on http://localhost:${PORT}`);
+});
+
+
