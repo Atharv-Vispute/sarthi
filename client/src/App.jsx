fa414b5e85a91d7a935dfaf6d86e869f7471977b
Atharv <visputeatharvvispute@gmail.com>
2025-09-13 10:31:46 +0530

    just commited

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,14 +1,30 @@
 import Hero from './components/Hero'
-import { useState } from 'react'
+import { useState,useEffect } from 'react'
 import './App.css'
 
 function App() {
   
+const [data,setdata] = useState("loading...")
+useEffect(()=>{
+  fetch("http://localhost:5000")
+  .then((responce)=>responce.json())
+  .then((result)=>setdata(result.message));
+
+},[]);
+
+
 
   return (
     <>
       <div>
+        
         <Hero></Hero>
+
+
+         <section style={{ padding: "20px" }}>
+        <h2>Frontend + Backend Connection</h2>
+        <p style={{ color: "blue" }}>{data}</p>
+      </section>
       </div>
     </>
   )
